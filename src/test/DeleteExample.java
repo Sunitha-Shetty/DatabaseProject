f97e5d85065ed746f4bce976bccd212b1a365338
Sunitha Shetty <shettysunitha2011@gmail.com>
2022-02-06 04:38:15 +0000

    Database Connection project

---
+++ src/test/DeleteExample.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteExample {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ecommerce", "root", "root");
+		
+		Statement stn = con.createStatement();
+		
+		stn.executeUpdate("delete from eproduct where ID =11");
+		
+		ResultSet result = stn.executeQuery("select * from eproduct");
+		
+		while(result.next())
+		{
+		//	System.out.println(result.getInt("ID"));
+			System.out.println(result.getString("name"));
+			
+		}
+		
+		con.close();
+		
+	}
+
+}
